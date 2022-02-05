74ad2fabd4bb2026a5c3a9168205cba6e7f57e4c
Mauricio Cevallos <mceval2@uic.edu>
2022-02-04 19:26:15 -0600

    implemented xyz directions

--- cubey.js
+++ cubey.js
@@ -1,14 +1,11 @@
 function setup(){
     createCanvas(windowWidth, windowHeight, WEBGL);
-    background(255,255,255);
+    background(0);
 }
 
 let z = 0;
 let y = 0;
 let x = 0;
-
-goingBack = false;
-goingForward = false; 
 
 //translate in relation to the last object that was created
 function draw() { 
@@ -22,14 +19,6 @@ function draw() {
     translate(x,y,z);
     box(25);
 
-    //trans = trans + 25;
-
-    // translate(0,0,25);
-    // box(25);
-
-    // translate(0,25);
-    // box(25);
-
 }
 
 function keyPressed() { 
@@ -37,12 +26,29 @@ function keyPressed() {
       background(255,255,255);
     }
 
-    if (keyCode === 83){ // S key
+    if (keyCode === 81){ // Q key goes forward on the Z axis
         z = z + 25;
     }
 
-    if (keyCode === 87){ // W key
+    if (keyCode === 69){ // E key goes backward in the Z axis
+        z = z - 25;
+    }
+
+    if (keyCode === 87){ // W key goes upwards in the Y axis
+        y = y - 25;
+    }
+
+    if (keyCode === 83){ // S key goes down in the Y axis
         y = y + 25;
     }
+
+    if (keyCode === 65){ // A key goes left on the X axis
+        x = x - 25;
+    }
+
+    if (keyCode === 68){ // D key goes right on the X axis
+        x = x + 25;
+    }
+     
 
   }
